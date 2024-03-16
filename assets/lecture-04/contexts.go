dba904489186277eadb52ba048e9e2c4bfff5347
Stanislav Zeman <zeman@standa.dev>
2024-03-16 21:12:51 +0100

    feat(lecture-04): add contexts

---
+++ assets/lecture-04/contexts.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand/v2"
+	"time"
+)
+
+// START OMIT
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 6*time.Second)
+	data := make(chan int, 4)
+	producerDone := make(chan bool)
+	consumerDone := make(chan bool)
+	go func() { // producer
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Producer cancelled!")
+				producerDone <- true
+				return
+			default:
+				data <- rand.IntN(1000)
+			}
+		}
+	}()
+
+	// MIDDLE OMIT
+
+	go func() { // consumer
+		for {
+			select {
+			case v := <-data:
+				fmt.Println("Procesing...")
+				time.Sleep(time.Second)
+				fmt.Printf("Processed: %s\n", v)
+			case <-ctx.Done():
+				fmt.Println("Consumer cancelled!")
+				consumerDone <- true
+				return
+			}
+		}
+	}()
+
+	<-producerDone
+	<-consumerDone
+}
+
+// END OMIT
