54f6cd6b5e2e51b72c10e31dd753f71f8d1edd4f
Stanislav Zeman <zeman@standa.dev>
2024-04-08 23:30:35 +0200

    feat(lecture-07): finish openapi slides

---
+++ assets/lecture-07/openapi/annotations.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// START OMIT
+
+// ShowAccount godoc
+// @Summary      Show an account
+// @Description  get string by ID
+// @Tags         accounts
+// @Produce      json
+// @Param        id   path      int  true  "Account ID"
+// @Success      200  {object}  model.Account
+// @Failure      400  {object}  httputil.HTTPError
+// @Failure      404  {object}  httputil.HTTPError
+// @Router       /accounts/{id} [get]
+func (c *Controller) ShowAccount(ctx *gin.Context) {
+	id := ctx.Param("id")
+	aid, err := strconv.Atoi(id)
+	if err != nil {
+		httputil.NewError(ctx, http.StatusBadRequest, err)
+		return
+	}
+	account, err := model.AccountOne(aid)
+	if err != nil {
+		httputil.NewError(ctx, http.StatusNotFound, err)
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+// END OMIT
