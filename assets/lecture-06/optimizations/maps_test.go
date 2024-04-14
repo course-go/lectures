3649548905bd00183af33dadcad2e95ea1e78766
Stanislav Zeman <zeman@standa.dev>
2024-04-14 12:10:23 +0200

    feat(lecture-06): finish cgo slides

--- assets/lecture-06/optimizations/maps_test.go
+++ assets/lecture-06/optimizations/maps_test.go
@@ -7,7 +7,40 @@ import (
 	"github.com/google/uuid"
 )
 
-// START OMIT
+// COMPOUND-KEY-VALUE START OMIT
+
+type value struct{}
+
+type key struct {
+	ID      int
+	payload [100]byte
+}
+
+// COMPOUND START OMIT
+
+func BenchmarkInsertIntoPreallocatedMapCompoundKey(b *testing.B) {
+	m := make(map[key]value, b.N)
+	for i := 0; i < b.N; i++ {
+		k := key{
+			ID: i,
+		}
+		m[k] = value{}
+	}
+}
+
+func BenchmarkInsertIntoEmptyMapCompoundKey(b *testing.B) {
+	m := map[key]value{}
+	for i := 0; i < b.N; i++ {
+		k := key{
+			ID: i,
+		}
+		m[k] = value{}
+	}
+}
+
+// COMPOUND END OMIT
+
+// UUID START OMIT
 
 func BenchmarkPreallocatedMapInsert(b *testing.B) {
 	m := make(map[uuid.UUID]time.Time, b.N)
@@ -31,4 +64,79 @@ func BenchmarkMapInsert(b *testing.B) {
 	}
 }
 
-// END OMIT
+// UUID END OMIT
+
+// SET-FILL START OMIT
+
+type ID int
+
+func genID(i int) ID {
+	return ID(3*i + 1)
+}
+
+func fillInMap(b *testing.B, n int) map[ID]struct{} {
+	b.StopTimer()
+
+	m := make(map[ID]struct{}, n)
+
+	for i := 0; i < n; i++ {
+		id := genID(i)
+		m[id] = struct{}{}
+	}
+
+	b.StartTimer()
+	return m
+}
+
+func fillInSlice(b *testing.B, n int) []ID {
+	b.StopTimer()
+
+	s := make([]ID, n)
+
+	for i := 0; i < n; i++ {
+		id := genID(i)
+		s[i] = id
+	}
+
+	b.StartTimer()
+	return s
+}
+
+// SET-FILL END OMIT
+// SET START OMIT
+
+func BenchmarkFindInMap(b *testing.B) {
+	s := fillInSlice(b, 1_000_000)
+	m := fillInMap(b, 1_000_000)
+	performBenchmarkFindInMap(b, m)
+	performBenchmarkFindInSlice(b, s)
+}
+
+func performBenchmarkFindInMap(b *testing.B, m map[ID]struct{}) {
+	items := len(m)
+	for i := 0; i < b.N; i++ {
+		_, found := m[genID(i%items)]
+		if !found {
+			b.Fatal("not found")
+		}
+	}
+}
+
+func performBenchmarkFindInSlice(b *testing.B, s []ID) {
+	items := len(s)
+	for i := 0; i < b.N; i++ {
+		found := false
+		id := genID(i % items)
+		for _, p := range s {
+			if p == id {
+				found = true
+				break
+			}
+		}
+		if !found {
+			b.Fatal("not found")
+		}
+	}
+}
+
+// SET END OMIT
