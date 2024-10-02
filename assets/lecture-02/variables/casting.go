7c1226380dde66d91dce79848285d482998fdceb
Stanislav Zeman <zeman@standa.dev>
2024-10-02 23:35:31 +0200

    feat(lecture-02): add sum type conversion example

--- assets/lecture-02/variables/casting.go
+++ assets/lecture-02/variables/casting.go
@@ -6,6 +6,12 @@ func main() {
 	var a float64
 	a = float64(5)
 	p := (*int)(nil)
-	s := "hello"
-	fmt.Printf("a: %f, p: %p, s: %s\n", a, p, s)
+
+	fmt.Printf("a: %f, p: %p", a, p)
+
+	x := byte(24)
+	y := int8(12)
+	z := 12 // int
+
+	fmt.Printf("sum: %d\n", x+byte(y)+byte(z))
 }
