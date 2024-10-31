3d2172b4be68968492ca3323bdf8ce35cd62eeae
Stanislav Zeman <zeman@standa.dev>
2024-10-31 13:43:03 +0100

    lecture-05: add expected result in test example

--- assets/lecture-05/testing/exported-add-separated-package_test.go
+++ assets/lecture-05/testing/exported-add-separated-package_test.go
@@ -1,13 +1,14 @@
 package numbers_test
 
 import (
-	"path/to/local/package/numbers"
 	"testing"
+
+	"path/to/local/package/numbers"
 )
 
 func TestAdd(t *testing.T) {
 	result := numbers.Add(1, 2)
-	if result != numbers.Add(1, 2) {
-		t.Error("1+2 should be 3, got ", result, "instead")
+	if result != 3 {
+		t.Error("1+2 should be 3, got:", result, "instead")
 	}
 }
