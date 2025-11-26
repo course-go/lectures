383b0201bd09a17fb9e64678f6c7f7e5d702acbf
Stanislav Zeman <zeman@standa.dev>
2025-11-27 00:31:32 +0100

    lecture-01: link lazo velkos rant video on cpp

---
+++ assets/lecture-01/c++/vector/main.cpp
@@ -0,0 +1,9 @@
+#include <algorithm>
+#include <vector>
+
+int main() {
+  int a;
+  std::vector<std::vector<int>> v;
+  std::vector<std::vector<int>>::const_iterator it =
+      std::find(v.begin(), v.end(), a);
+}
